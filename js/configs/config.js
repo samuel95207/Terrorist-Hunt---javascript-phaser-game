a5f59b6d3abf6ea9ecaedbccb15bb351af6e1d7f
Samuel Huang <lspss95207@gmail.com>
2020-06-18 15:27:13 +0800

    Separate weapon config

---
+++ js/configs/config.js
@@ -0,0 +1,42 @@
+const gameConfig = {}
+
+gameConfig.height = window.innerHeight-120;
+gameConfig.width = window.innerWidth-120;
+
+console.log(gameConfig.width)
+
+//player configs
+gameConfig.playerAcceleration = 70;
+gameConfig.playerFriction = 30;
+gameConfig.playerMaxSpeed = 600;
+gameConfig.jumpVelocity = 1100;
+gameConfig.playerLives = 3;
+
+//physics configs
+gameConfig.gravity = 3000;
+gameConfig.enemyFriction = 30;
+
+
+
+//keyboard config
+gameConfig.keyConfig = {
+    up: 'UP',
+    down: 'DOWN',
+    left: 'LEFT',
+    right: 'RIGHT',
+    jump: 'UP',
+    fire: 'z',
+    pickup: 'c'
+}
+
+
+gameConfig.enemy = {
+    ordinaryEnemy: {
+        acceleration: 300,
+        maxSpeed: 500,
+        sight: 400,
+        fireProbability: 0.3,
+        fireDelay: 500,
+        weapon: "assaultRifle"
+    }
+}
